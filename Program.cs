f83a65a56557bf4b6c543cc909ee76e5703ac1a8
Luiz <38606125814@info.local>
2023-04-04 08:43:24 -0300

    Terminei a atividade

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(@$"
+Escolha sua bebida:
+- Guarana (digite 'G')
+- Coco-cola (digite 'C')
+- Agua (digite 'A')
+- Cerveja (digite 'F')
+");
+
+        char bebida = char.Parse(Console.ReadLine().ToUpper());
+
+        switch (bebida)
+        {
+            case 'G':
+            Console.WriteLine($"Voce escolheu Guarana. Gostaria de adicionar gelo? ('s' para sim, 'n' para nao)");
+            char gelo = char.Parse(Console.ReadLine().ToLower());
+            if (gelo == 's')
+            {
+                Console.WriteLine($"Colocarei gelo. Sua bebida esta sendo feita ...");
+            }
+            else
+            {
+                Console.WriteLine($"Sua bebida esta sendo feita ...");
+                
+            }
+            break;
+
+            case 'C':
+            Console.WriteLine($"Voce escolheu Coca-cola. Gostaria de adicionar gelo? ('s' para sim, 'n' para nao)");
+            char gelo2 = char.Parse(Console.ReadLine().ToLower());
+            if (gelo2 == 's')
+            {
+                Console.WriteLine($"Colocarei gelo. Sua bebida esta sendo feita ...");
+            }
+            else
+            {
+                Console.WriteLine($"Sua bebida esta sendo feita ...");
+            }
+            break;
+
+            case 'A':
+            Console.WriteLine($"Voce escolheu Agua, a melhor bebida. Gostaria de adicionar gelo? ('s' para sim, 'n' para nao)");
+            char gelo3 = char.Parse(Console.ReadLine().ToLower());
+            if (gelo3 == 's')
+            {
+                Console.WriteLine($"Colocarei gelo. Sua bebida esta sendo feita ...");
+            }
+            else
+            {
+                Console.WriteLine($"Sua bebida esta sendo feita ...");
+                
+            }
+            break;
+
+            case 'F':
+            Console.WriteLine($"Voce escolheu Agua, a melhor bebida. Gostaria de adicionar gelo? ('s' para sim, 'n' para nao)");
+            char gelo4 = char.Parse(Console.ReadLine().ToLower());
+            if (gelo4 == 's')
+            {
+                Console.WriteLine($"Colocarei gelo. Sua bebida esta sendo feita ...");
+            }
+            else
+            {
+                Console.WriteLine($"Sua bebida esta sendo feita ...");
+            }
+            break;
+            
+        }
+    }
+}
